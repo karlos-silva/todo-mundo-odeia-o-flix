5c513418f07d658f8df9aef6a7d1a5b89a8f8bf9
karlos-silva <karlossilva194@gmail.com>
2020-07-29 06:33:51 -0300

    Adiciona links dos videos do Chris

--- src/App.js
+++ src/App.js
@@ -13,10 +13,24 @@ function App() {
       <BannerMain
       videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
       url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"O que é Front-End"}
       />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
